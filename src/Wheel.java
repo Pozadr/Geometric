fb21ae74d178107be29f9aabd3678f0b753b669f
Pozadr <adpozor@gmail.com>
2020-04-16 11:27:22 +0200

    Init commit

---
+++ src/Wheel.java
@@ -0,0 +1,13 @@
+public class Wheel {
+    private double r;
+
+    Wheel
+
+    public double getFiled(){
+        return Math.PI*r*r;
+    }
+
+    public double getPerimeter(){
+        return 2*Math.PI*r;
+    }
+}
